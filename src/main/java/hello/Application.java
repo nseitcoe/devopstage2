02dd90c743218350c4baec56ccd1ab3f53bd8ef7
nseitcoe <122255850+nseitcoe@users.noreply.github.com>
2023-01-09 20:50:33 +0530

    With HI

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Mai ban gya devops Engineer!!";
+        return "Mai ban gya devops Engineer!! HI";
     }
 
     public static void main(String[] args) {
