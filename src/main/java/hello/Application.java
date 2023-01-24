8252d94a8fd56c96e7edf8dd248c694b75d50d3e
nseitcoe <122255850+nseitcoe@users.noreply.github.com>
2023-01-24 13:15:20 +0530

    NEW COMIT

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Mai ban gya devops Engineer!! Helo 2.2";
+        return "Mai ban gya devops Engineer!! nEW COMIT";
     }
 
     public static void main(String[] args) {
