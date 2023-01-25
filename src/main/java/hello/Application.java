34bc83230d35553465fd52fc12402a625a05fbae
nseitcoe <122255850+nseitcoe@users.noreply.github.com>
2023-01-25 12:18:57 +0530

    Update Application.java
    Final Changes

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Mai ban gya devops Engineer!! nEW COMIT";
+        return "Azure DevOps is the best";
     }
 
     public static void main(String[] args) {
